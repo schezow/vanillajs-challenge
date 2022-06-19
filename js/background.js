cd4464dcc96afae2d6c56db8964ff93f6e8eb4a6
hyunchul.yi <hyunchul.yi@lge.com>
2022-06-20 01:18:40 +0900

    DONE

---
+++ js/background.js
@@ -0,0 +1,10 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+bgImage.classList.add("bgImage"); // image에 bgImage 클래스 추가 
+
+document.body.appendChild(bgImage);
